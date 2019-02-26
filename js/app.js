7ddce7ddd7358967ee8fbafb0a145987a09258d4
Daksa Media.Inc <daksamedia.id@gmail.com>
2019-02-26 20:50:40 +0700

    init commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Dom7
+var $$ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  theme: theme,
+  actions: {
+    backdrop: false,
+  },
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes,
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
+
+var room_id = "shj1820938isdadiauq"
+var DBref = new Firebase('https://gelaro-dev.firebaseio.com/'+room_id);
